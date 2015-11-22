ccd680f967371fd65a767e0bd791d4fa2102bcc5
Neeraj <soccernee@yahoo.com>
2015-11-21 18:01:58 -0800

    update RAILS models

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,4 @@
 class Shipment < ActiveRecord::Base
-  attr_accessor :name
+  attr_accessor :name, :receiver, :destination, :description, :bol, :custody, :hasDamaged, :hasSeparated, :isLate, :latitude, :longitude
   has_many :pallet
 end
