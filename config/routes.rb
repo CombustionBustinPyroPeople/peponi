1bb6045cd4b4cdef5b1b9c973257745b47152872
Neeraj <nchandra324@gmail.com>
2015-11-21 23:43:37 -0800

    forgot the cambridge comma

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Ebenezer::Application.routes.draw do
 
   namespace :api, defaults: {format: "json"} do
     namespace :v1 do
-      get 'pallets'         to: 'pallets#get_all'
-      get 'pallets/:id'     to: 'pallets#get'
+      get 'pallets',         to: 'pallets#get_all'
+      get 'pallets/:id',     to: 'pallets#get'
 
-      get 'shipments'       to: 'shipments#get_all'
-      get 'shipments/:id'   to: 'shipments#get'
+      get 'shipments',       to: 'shipments#get_all'
+      get 'shipments/:id',   to: 'shipments#get'
     end
   end
 
